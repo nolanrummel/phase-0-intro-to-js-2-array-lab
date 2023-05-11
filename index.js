b5342add69d4f6efab634784723f86c63c02863e
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-11 09:05:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) { cats.push(name) };
+function destructivelyPrependCat(name) { cats.unshift(name) };
+function destructivelyRemoveLastCat() { cats.pop(name) };
+function destructivelyRemoveFirstCat() { cats.shift(name) };
+function appendCat(name) { const appendCat = [...cats, name]; return appendCat; };
+function prependCat(name) { const prependCat = [name, ...cats]; return prependCat; };
+function removeLastCat() { const removeLastCat = cats.slice(); removeLastCat.pop(); return removeLastCat };
+function removeFirstCat() { const removeFirstCat = cats.slice(1); return removeFirstCat };
+/* function removeLastCat() { const removeLastCat = cats.splice(0,1) return removeLastCat } */
