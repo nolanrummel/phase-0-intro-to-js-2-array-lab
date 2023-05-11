0254e0302a78369984e1c0c4c2121e7c64e3822f
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-11 09:09:40 -0500

    complete lab updated

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const cats = ["Milo", "Otis", "Garfield"];
-function destructivelyAppendCat(name) { cats.push(name) };
-function destructivelyPrependCat(name) { cats.unshift(name) };
-function destructivelyRemoveLastCat() { cats.pop(name) };
-function destructivelyRemoveFirstCat() { cats.shift(name) };
-function appendCat(name) { const appendCat = [...cats, name]; return appendCat; };
-function prependCat(name) { const prependCat = [name, ...cats]; return prependCat; };
-function removeLastCat() { const removeLastCat = cats.slice(); removeLastCat.pop(); return removeLastCat };
-function removeFirstCat() { const removeFirstCat = cats.slice(1); return removeFirstCat };
-/* function removeLastCat() { const removeLastCat = cats.splice(0,1) return removeLastCat } */
+function destructivelyAppendCat(name) { 
+    cats.push(name) };
+
+function destructivelyPrependCat(name) { 
+    cats.unshift(name) };
+
+function destructivelyRemoveLastCat() { 
+    cats.pop(name) };
+
+function destructivelyRemoveFirstCat() 
+{ cats.shift(name) };
+
+function appendCat(name) { 
+    const appendCat = [...cats, name]; 
+    return appendCat; };
+
+function prependCat(name) { 
+    const prependCat = [name, ...cats]; 
+    return prependCat; };
+    
+function removeLastCat() { 
+    const removeLastCat = cats.slice(); 
+    removeLastCat.pop(); 
+    return removeLastCat };
+
+function removeFirstCat() { 
+    const removeFirstCat = cats.slice(1); 
+    return removeFirstCat };
+
+/* 
+function removeLastCat() { 
+    const removeLastCat = cats.splice(0,1)
+     return removeLastCat }
+*/
